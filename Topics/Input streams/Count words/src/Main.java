35a38a6a640a02bdfd5407da1a8dc9ab22c7aa37
grzesiakmarcin <mk.grzesiak@gmail.com>
2021-05-03 11:07:19 +0200

    sukces

--- Topics/Input streams/Count words/src/Main.java
+++ Topics/Input streams/Count words/src/Main.java
@@ -5,19 +5,28 @@ class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String input = reader.readLine();
-        int number = 0;
-        char[] arrayOfImput = input.toCharArray();
-        if (arrayOfImput.length > 0) {
-            number++;
-            for (int i = 0; i < arrayOfImput.length; i++) {
+        int counter = 0;
+//        char[] arrayOfImput = input.toCharArray();
+//        if (arrayOfImput.length > 0) {
+//
+//            counter++;
+//            for (int i = 0; i < arrayOfImput.length; i++) {
+//
+//                if ((arrayOfImput[i] == ' ') && (arrayOfImput[i + 1] != ' ')) {
+//                    counter++;
+//                }
+//            }
+//        }
 
-                if ((arrayOfImput[i] == ' ') && (arrayOfImput[i + 1] != ' ')) {
-                    number++;
-                }
+        String[] arrayOfImput = input.split(" ");
+        for (String element : arrayOfImput) {
+            element.trim();
+            if (!element.equals("")) {
+                counter++;
             }
         }
 
-        System.out.println(number);
+        System.out.println(counter);
 
         reader.close();
     }
